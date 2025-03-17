a7fea41689690eae7fb52fa1fe0a963454c4992b
Antony Vilanova <mc6619002@gmail.com>
2025-03-17 10:34:54 -0300

    dudaaa

--- BackSRC/AlmoxarifadoBackSLN/AlmoxarifadoBackAPI/obj/Debug/net7.0/AlmoxarifadoBackAPI.AssemblyInfo.cs
+++ BackSRC/AlmoxarifadoBackSLN/AlmoxarifadoBackAPI/obj/Debug/net7.0/AlmoxarifadoBackAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlmoxarifadoBackAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8be8fabe6889439def37b0b1c0b4568be4dcf85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f5177d4577937fbf5db86f4d5874fdaf198503a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlmoxarifadoBackAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlmoxarifadoBackAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FrontSRC/src/AlmoxafiradoFrontSLN/AlmoxafiradoFront/obj/Debug/net8.0/AlmoxafiradoFront.AssemblyInfo.cs
+++ FrontSRC/src/AlmoxafiradoFrontSLN/AlmoxafiradoFront/obj/Debug/net8.0/AlmoxafiradoFront.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlmoxafiradoFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4505a793cb502b0ef643ceb1ffcf9f449667146c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f5177d4577937fbf5db86f4d5874fdaf198503a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlmoxafiradoFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlmoxafiradoFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
