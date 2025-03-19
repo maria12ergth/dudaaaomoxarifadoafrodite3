9b85a09210b8abd48ce288b6b4a65c3dffa242e9
viniciuss1812 <mc6619002@gmail.com>
2025-03-19 16:59:08 -0300

    sou lindaaa

--- FrontSRC/src/AlmoxafiradoFrontSLN/AlmoxafiradoFront/obj/Debug/net8.0/AlmoxafiradoFront.AssemblyInfo.cs
+++ FrontSRC/src/AlmoxafiradoFrontSLN/AlmoxafiradoFront/obj/Debug/net8.0/AlmoxafiradoFront.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlmoxafiradoFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f5177d4577937fbf5db86f4d5874fdaf198503a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7fea41689690eae7fb52fa1fe0a963454c4992b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlmoxafiradoFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlmoxafiradoFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
